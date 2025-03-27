b1e051a4b89c06cf26320770819ee4bb0dc315c6
Artem Lapkin <art@khadas.com>
2025-03-27 10:58:41 +0000

    scripts/mkimg.khadas.sh: update
    Signed-off-by: Artem Lapkin <art@khadas.com>

--- scripts/mkimg.khadas.sh
+++ scripts/mkimg.khadas.sh
@@ -34,7 +34,7 @@ build_kernel() {
 	--keys-dir "$APKROOT/etc/apk/keys" \
 	--flavor "$_flavor" \
 	--arch "$ARCH" \
-	--package "$_pkgs" \
+	--package "$_pkgs mkinitfs" \
 	--feature "$initfs_features" \
 	--modloopfw "$modloopfw" \
 	--repositories-file "$APKROOT/etc/apk/repositories" \
@@ -56,7 +56,6 @@ section_kernels() {
 	
 	local id=$( (echo "$initfs_features::$_hostkeys" ; apk fetch --root "$APKROOT" --simulate alpine-base $_pkgs | sort) | checksum)
 	build_section kernel $ARCH $_f $id $_pkgs
-	exit 1
     done
 }
 
@@ -68,9 +67,12 @@ profile_khadas() {
 	arch="aarch64"
 	kernel_flavors="khadas-edge2-oowow-neo-bin"
 #	kernel_flavors="rpi"
-	kernel_cmdline="console=tty1"
-	initfs_features="base squashfs mmc usb kms dhcp https"
+	#kernel_cmdline="console=tty1"
+	kernel_append="modules=loop,squashfs,sd-mod,usb-storage debug_init "
+	kernel_cmdline="net.ifnames=0 video=HDMI-A-1:1920x1080@60 panic=10 fbcon=font:TER16x32 earlycon=uart8250,mmio32,0xfeb50000 console=ttyFIQ0"
+	initfs_features="base squashfs mmc usb kms dhcp https "
 	hostname="khadas"
+	apks="$apks sfdisk"
 	grub_mod=
 }
 
@@ -79,4 +81,64 @@ profile_khadas_img() {
 	title="Khadas Edge2 Disk Image"
 	image_name="alpine-khadas"
 	image_ext="img.gz"
+}
+
+create_image_imggz() {
+    echo "KHADAS img gz"
+    sync "$DESTDIR"
+    CMD du -L -k -s "$DESTDIR"
+    local part1_start=24576 # 512 byte blocks
+
+    local image_size=$(du -L -k -s "$DESTDIR" | awk '{print $1 + 8192}' ) # 1k bytes blocks
+    local imgfile="${OUTDIR}/${output_filename%.gz}"
+    image_size=$((image_size+part1_start*2))
+
+    echo "IMAGE_SIZE: $image_size"
+
+    CMD dd if=/dev/zero of="$imgfile" bs=1M count=$(( image_size / 1024 ))
+
+    cat <<EOF | tee /dev/stderr | CMD sfdisk "$imgfile"
+label: dos
+
+PART1 : start=$part1_start, type=e
+EOF
+
+    local part1_img="$imgfile@@$((part1_start*512))"
+    CMD mformat -i "$part1_img" -N 0 ::
+    (
+    CMD cd $DESTDIR
+    CMD mcopy -s -i "$part1_img" * .alpine-release ::
+    ) || return 1
+
+    local FDT=boot/dtbs-$kernel_flavors/rk3588s-khadas-edge2.dtb
+    
+    CMD mmd -i "$part1_img" ::boot/extlinux
+    cat <<EOF | tee /dev/stderr | CMD mcopy -i "$part1_img" - ::boot/extlinux/extlinux.conf
+#MENU background /boot/logo.bmp
+LABEL oowow-neo
+    LINUX /boot/vmlinuz-$kernel_flavors
+    INITRD /boot/initramfs-$kernel_flavors
+    FDT /$FDT
+    APPEND $kernel_append $kernel_cmdline
+
+timeout 10
+default oowow
+EOF
+
+    cat <<EOF | tee /dev/stderr | CMD mcopy -i "$part1_img" - ::boot/uEnv.txt
+#
+# uEnv.txt
+#
+EOF
+
+    cat <<EOF | tee /dev/stderr | CMD mcopy -i "$part1_img" - ::$FDT.overlay.env
+# fdt overlay config file - example
+# default
+fdt_overlays_dir=
+fdt_overlays=
+EOF
+
+#   CMD mdir -/ -i "$part1_img" ::
+    echo "Compressing $imgfile..."
+    CMD pigz -v -k -f -9 "$imgfile" || CMD gzip -f -9 "$imgfile"
 }
